daebe9ba7496f54f5b25c26892841161c96e2ff9
Zeynep BARAN <zbaran@outlook.com.tr>
2025-05-10 19:54:45 +0300

    core layer added

---
+++ blogSite.Core/Entity/CoreEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace blogSite.Core.Entity
+{
+    public class CoreEntity
+    {
+        public int ID{ get; set; }
+        public DateTime? CreateTime { get; set; }
+        public DateTime? UpdateTime{ get; set; }
+    }
+}
