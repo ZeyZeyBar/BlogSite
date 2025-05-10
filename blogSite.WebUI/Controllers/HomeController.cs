f9963974d283efca7dcd50d82d4c2e34d3b3fa2b
Zeynep BARAN <zbaran@outlook.com.tr>
2025-05-10 23:14:00 +0300

    NOT ADDED

--- blogSite.WebUI/Controllers/HomeController.cs
+++ blogSite.WebUI/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 
 namespace blogSite.WebUI.Controllers
 {
+    //ADMİN SAYFASINA GEÇTİN
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
