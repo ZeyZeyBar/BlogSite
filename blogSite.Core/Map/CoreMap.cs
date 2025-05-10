3e9d7d416d1c48771d120f8e38c7ec2d42af11f4
Zeynep BARAN <zbaran@outlook.com.tr>
2025-05-10 21:23:48 +0300

    core layer method created

---
+++ blogSite.Core/Map/CoreMap.cs
@@ -0,0 +1,16 @@
+﻿using blogSite.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace blogSite.Core.Map
+{
+    public abstract class CoreMap<T> : IEntityTypeConfiguration<T> where T : CoreEntity
+    {
+        public void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(x=>x.ID);
+            builder.Property(x=>x.CreateTime).IsRequired(false);
+            builder.Property(x=>x.UpdateTime).IsRequired(false);
+        }
+    }
+}
