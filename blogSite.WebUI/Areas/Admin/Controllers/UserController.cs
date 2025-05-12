2b78b1be8cdc1640b7620060e8d38f5593395aad
Zeynep BARAN <zbaran@outlook.com.tr>
2025-05-12 16:50:53 +0300

    update method aded for admin panel

--- blogSite.WebUI/Areas/Admin/Controllers/UserController.cs
+++ blogSite.WebUI/Areas/Admin/Controllers/UserController.cs
@@ -54,5 +54,54 @@ namespace blogSite.WebUI.Areas.Admin.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult UpdateUser(int id, User usernew)
+        {
+            var user = _userDb.GetById(id);
+            if (usernew != null)
+            {
+                user.UpdateTime = DateTime.Now;
+                user.Name = usernew.Name;
+                user.Surname = usernew.Surname;
+                user.UserName = usernew.UserName;
+                user.Password = usernew.Password;
+                user.IsActive = usernew.IsActive;
+                    
+
+                var result = _userDb.Update(user);
+                if (result)
+                {
+                    TempData["SuccesMessage"] = "Kullanıcı içeriği başarılı bir şekilde güncellendi.";
+                    return RedirectToAction("GetAllUser");
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Hata oluştu tekrar deneyin";
+                }
+            }
+            else
+            {
+                // Eğer model geçerli değilse, ModelState hatalarını logla
+                var errors = ModelState.Values.SelectMany(v => v.Errors);
+                foreach (var error in errors)
+                {
+                    Console.WriteLine($"Hata: {error.ErrorMessage}");
+                }
+            }
+            return View(user); ;
+        }
+        [HttpGet]
+        public IActionResult UpdateUser(int id)
+        {
+            var user = _userDb.GetById(id);
+
+            if (user == null)
+            {
+                return NotFound(); // bulunamazsa 404 döndür
+            }
+            // bulundu, View'a Model olarak gönderiyoruz
+            return View(user);
+
+        }
     }
 }
